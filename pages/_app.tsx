0b81e9b21ea7d46c0a86eb8ba304ccfd41f1bf51
CerealeZ <samuelharo.dev@gmail.com>
2024-05-23 22:35:51 -0300

    build: add tailwind and prettier dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/normalize.css"
+import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import { Lato } from "next/font/google"
 import { AppProvider } from "@/src/contexts/AppContext"
